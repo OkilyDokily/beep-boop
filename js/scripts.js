8af45cade76a79d26b9177c9f04a1c18469a1843
me <me@me.me.com>
2020-05-13 15:45:06 -0700

    implement the second spec - convert a number to a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 function convertToRange(number){
-  var range = new Array(number + 1).fill(undefined).map((_,i)=>i);
-  
+  return new Array(number + 1).fill(undefined).map((_,i)=>i);
+}
+
+function convertNumberToString(number){
+  return number.toString();
 }
 
 
 $(document).ready(function(){
   convertToRange(5);
+  console.log(convertNumberToString(45));
 })
