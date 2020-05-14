be8e4c0de10bb6e3282974f11299ca7da5eaaef6
me <me@me.me.com>
2020-05-14 13:10:58 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,13 @@ function convertToRange(number){
   return new Array(number + 1).fill(undefined).map((_,i)=>i);
 }
 
-function convertNumberToString(number){
-  return number.toString();
-}
-
-function convertStringToArrayOfChars(number){
-  var string = convertNumberToString(number)
-  return string.split("");
-}
-
-function filterTheArrayOfChars(number){
-  var arrayOfChars = convertStringToArrayOfChars(number);
-  return arrayOfChars.filter(function(item){
+function getLargestValueBetweenOneAndThree(number){
+  var filteredArray = number.toString().split("").filter(function (item){
     return /[1-3]/.test(item);
-  })
-}
-
-function findMaximumValueInFilteredArray(number){
-  var filteredArray = filterTheArrayOfChars(number);
-  console.log(filteredArray)
-  return Math.max(...filteredArray)
+  });
+  return Math.max(...filteredArray);
 }
 
 $(document).ready(function(){
-  console.log(findMaximumValueInFilteredArray(1223345));
+  console.log(getLargestValueBetweenOneAndThree(1223345));
 })
