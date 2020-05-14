0eca8a1e9a04c6538191279fb22a80c45127a3b8
me <me@me.me.com>
2020-05-14 13:23:21 -0700

    implement spec 6 and 7 - find the appropriate return value and map the previous steps

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 function convertToRange(number){
-  return new Array(number + 1).fill(undefined).map((_,i)=>i);
+  return new Array(number + 1).fill(undefined).map(function (_,i){
+    var filteredArray = i.toString().split("").filter(function (item){
+      return /[1-3]/.test(item);
+    });
+    var largest = Math.max(...filteredArray);
+    switch(largest){
+      case 3:
+        return "I'm sorry, Dave. I'm afraid I can't do that."
+      case 2:
+        return "Boop!"
+      case 1:
+        return "Beep!"
+      default:
+        return i;
+    }
+  });
 }
 
-function getLargestValueBetweenOneAndThree(number){
-  var filteredArray = number.toString().split("").filter(function (item){
-    return /[1-3]/.test(item);
-  });
-  return Math.max(...filteredArray);
-}
+
 
 $(document).ready(function(){
-  console.log(getLargestValueBetweenOneAndThree(1223345));
+  console.log(convertToRange(5));
 })
