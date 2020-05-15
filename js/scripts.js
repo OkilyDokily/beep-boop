b97ccdc3e6c2eda2cbfe3c2393b1e38fff818db6
me <me@me.me.com>
2020-05-15 14:10:59 -0700

    use larger well

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//business logic
 function convertNumberToBoopSequence(number){
   return new Array(number + 1).fill(undefined).map(function (_,i){
     var filteredArray = i.toString().split("").filter(function (item){
@@ -19,56 +20,55 @@ function convertNumberToBoopSequence(number){
 
 
 $(document).ready(function(){
- $("form").submit(function(e){
-   e.preventDefault();
-   var intervalId;
-   var input = $("input").val();
-   var results = convertNumberToBoopSequence(parseInt(input));
-   
-   var i = 0;
+  $("form").submit(function(e){
+    clearResults();
+    e.preventDefault();
+    var intervalId;
+    var input = $("input").val();
+    var results = convertNumberToBoopSequence(parseInt(input));
+    
 
-   function displayImage(image){
-    $("img").remove();
-    switch (image){
-      case "Beep!":
-        $("div.image").append("<img src='img/beep.jpg'>");
-        break;
-      case "Boop!":
-        $("div.image").append("<img src='img/boop.jpg'>");
-        break;
-      case "I'm sorry, Dave. I'm afraid I can't do that.":
-        var i = 0;
-        requestAnimationFrame(function callback(){
-          if (i < 15){
-              console.log("call back")
-              $("body").css("backgroundColor","red");
-              i++;
-              requestAnimationFrame(callback);
-            }
-            else{
-              $("body").css("backgroundColor","aliceblue")
-            }   
-        });
-        
-       
+    
+    function displayImage(image){
+      $("img").remove();
+      switch (image){
+        case "Beep!":
+          $("div.image").append("<img src='img/beep.jpg'>");
+          break;
+        case "Boop!":
+          $("div.image").append("<img src='img/boop.jpg'>");
+          break;
+        case "I'm sorry, Dave. I'm afraid I can't do that.":
+          var i = 0;
+          requestAnimationFrame(function callback(){
+            if (i < 15){
+                console.log("call back")
+                $("body").css("backgroundColor","red");
+                i++;
+                requestAnimationFrame(callback);
+              }
+              else{
+                $("body").css("backgroundColor","aliceblue")
+              }   
+          });
+      }
     }
-   }
 
-
-   function code(){
-    displayImage(results[0]);
-    $("#results").prepend("<p>" + "<span>" + (i++) + ": " + "</span>"  + results[0]+ "</p>");
-    results.shift();
-    if (results.length === 0){
-      clearInterval(intervalId);
+    var i = 0;
+    function code(){
+      displayImage(results[0]);
+      $("#results").prepend("<p>" + "<span>" + (i++) + ": " + "</span>"  + results[0]+ "</p>");
+      results.shift();
+      if (results.length === 0){
+        clearInterval(intervalId);
+      }
+      if($("#results").children().length === 11){
+        $("#results p:nth-of-type(10)").remove();
+      }
     }
-    if($("#results").children().length === 11){
-      $("#results p:nth-of-type(10)").remove();
-    }
-  }
-  intervalId = setInterval(code,500);
-  
-   
-   
- })
+    intervalId = setInterval(code,500); 
+    function clearResults(){
+      $("p").remove();
+    }    
+  })
 })
